b32d100452363386ff3eb58a427427273953f8fc
Aditi-Mane <143419307+Aditi-Mane@users.noreply.github.com>
2026-01-20 16:15:43 +0530

    Setup React frontend with Tailwind

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+
+
+function App() {
+  return (
+    <>
+    <div className="min-h-screen bg-slate-900 flex items-center justify-center">
+      <h1 className="text-3xl font-bold underline text-amber-400">
+        Hello world!
+      </h1>
+    </div>
+    </>
+  )
+}
+
+export default App
