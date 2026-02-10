06cfa11d378768f44630e62ba60c300d999ffa5b
Aditi-Mane <143419307+Aditi-Mane@users.noreply.github.com>
2026-02-10 22:56:32 +0530

    feat(auth-frontend): Verify OTP page basic UI buildup

--- frontend/src/pages/auth/VerifyOtp.jsx
+++ frontend/src/pages/auth/VerifyOtp.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
+import AuthLayout from '../../components/auth/AuthLayout.jsx'
+import AuthCard from '../../components/auth/AuthCard.jsx'
+import AuthHeader from '../../components/auth/AuthHeader.jsx'
+import PrimaryButton from '../../components/auth/PrimaryButton.jsx'
+import InputField from '../../components/auth/InputField.jsx'
+import { GrSecure } from "react-icons/gr";
 
 const VerifyOtp = () => {
   return (
-    <div>
-      
-    </div>
+    <AuthLayout>
+      <AuthCard>
+        <AuthHeader title = "OTP verification" subtitle = "Please enter the OTP sent to your registered email."/>
+
+        <InputField icon={GrSecure} type="text" placeholder="Enter OTP"/>
+
+        <PrimaryButton>Verify OTP</PrimaryButton>
+      </AuthCard>
+    </AuthLayout>
   )
 }
 
