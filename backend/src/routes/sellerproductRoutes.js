e8dcd834469bfdd4e5dd1172e2ec4d91d894b00e
Aditi-Mane <143419307+Aditi-Mane@users.noreply.github.com>
2026-02-19 16:11:35 +0530

    required packages downloaded

--- backend/src/routes/sellerproductRoutes.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router();
-const { addProduct, getProducts } = require("../controllers/productController");
-
-router.post("/", addProduct);
-router.get("/", getProducts);
-
-module.exports = router;
--- frontend/src/pages/Marketplace/seller-module/SellerProducts.jsx
+++ frontend/src/pages/Marketplace/seller-module/SellerProducts.jsx
@@ -3,9 +3,10 @@ import React from 'react'
 const SellerProducts = () => {
   return (
     <div>
-      Products
+      
     </div>
   )
 }
 
 export default SellerProducts
+
