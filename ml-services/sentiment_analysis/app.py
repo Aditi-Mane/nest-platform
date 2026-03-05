6efbf490a05f844fc2d2a8512cd8078ac65f48c8
sidd51 <siddhiborawake2004@gmail.com>
2026-03-05 14:00:20 +0530

    feat(reviews): implement product review system with sentiment analysis

--- ml-services/sentiment_analysis/app.py
+++ ml-services/sentiment_analysis/app.py
@@ -2,41 +2,28 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from transformers import pipeline
 
-app=FastAPI()
+app = FastAPI()
 
-#load sentiment pipeline(pretrained)
-sentiment_model=pipeline("sentiment-analysis")
+sentiment_model = pipeline("sentiment-analysis")
 
-#Request Body Schema
 class ReviewRequest(BaseModel):
-  text:str
+    text: str
 
-#Response Schema
-class SentimentResponse(BaseModel):
-  sentiment: str
-  confidence: float
-
-@app.get("/")
-def home():
-    return {"message: Sentiment API running"}
-
-@app.post("/predict-sentiment", response_model=SentimentResponse)
+@app.post("/predict-sentiment")
 def predict_sentiment(request: ReviewRequest):
-   result=sentiment_model(request.text)[0]
 
-   label=result["label"] # POSITIVE / NEGATIVE
+    result = sentiment_model(request.text)[0]
 
-   score=float(result["score"])
+    label = result["label"]
+    score = float(result["score"])
 
-   # Convert into your categories
-   if label == "POSITIVE":
+    if label == "POSITIVE":
         sentiment = "positive"
-   else:
+    else:
         sentiment = "negative"
 
-   return {
+    return {
         "sentiment": sentiment,
         "confidence": score
     }
-
 
