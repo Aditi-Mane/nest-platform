6babd2ed0fdc0a0ec796bd35cd97a57a917d333f
Pranoti8012 <pranotichakwate2004@gmail.com>
2026-03-25 01:35:07 +0530

    feat(seller-module) - Seller Analytics UI

--- frontend/src/pages/Marketplace/seller-module/SellerSettings.jsx
+++ frontend/src/pages/Marketplace/seller-module/SellerSettings.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import { useState, useEffect } from "react";
 import axios from "axios";
 import { MdDelete, MdEdit, MdSwapHoriz } from "react-icons/md";
 
